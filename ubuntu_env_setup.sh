b020350d1b1969b8db3b221086b8fe69ebacdce3
snjianxiao <717290568@qq.com>
2018-03-12 17:25:03 +0800

    上传

---
+++ ubuntu_env_setup.sh
@@ -0,0 +1,16 @@
+
+
+
+apt-get update
+apt-get install python-pip -y
+apt-get install subversion -y
+pip install -U setuptools
+pip install -U pip
+pip install matplotlib -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install numpy -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install xlrd -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install pandas -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install seaborn scipy  -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install -U ipython
+
+
